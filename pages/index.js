59b468299f44817557ca6fb920151d6840a7da42
Brenio Medeiros <brenio@live.com>
2021-07-19 19:18:41 -0300

    inicio do alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,75 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/components/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+function ProfileSidebar(props) {
+  return (
+    <Box>
+      <img src={ `https://github.com/${props.githubUser}.png` } style={{ borderRadius: '8px' }}/>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+
+  const user = 'mrBrenio';
+  const pessoasFav = ['juunegreiros', 'omariosouto', 'peas', 'rafaballerini', 'marcobrunodev', 'felipefialho']
+
+  return (
+    <>
+      
+      <AlurakutMenu />
+      <MainGrid>
+
+        {/* <Box style="grid-area: profileArea"> */}
+
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={ user } />
+        </div>
+
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem-vindo (a) {user}
+            </h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <Box>
+            <h2>Amigos</h2>
+          </Box>
+
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da Comunidade ({pessoasFav.length})
+            </h2>
+
+            <ul>  
+              {pessoasFav.map((itemAtual) => {
+                return(
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+
+          </ProfileRelationsBoxWrapper>
+        </div>
+
+      </MainGrid>
+
+    </>
+  )
 }
